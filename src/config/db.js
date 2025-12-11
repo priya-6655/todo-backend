f884d80918bf9a46f55f2c1451fd9e5335ee66e0
Shanmuga Priya <shanmugapriya6655@gmail.com>
2025-12-11 21:38:59 +0530

    db sync issue fixes

--- src/config/db.js
+++ src/config/db.js
@@ -37,10 +37,11 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     },
     dialectOptions: {
         connectTimeout: 60000,
-        ssl: mode === 'LIVE' ? {
-            require: true,
-            rejectUnauthorized: false
-        } : false
+        ssl: false
+        // mode === 'LIVE' ? {
+        //     require: true,
+        //     rejectUnauthorized: false
+        // } : false
     }
 })
 
