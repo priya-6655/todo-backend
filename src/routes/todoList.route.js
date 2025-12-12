105f6576679a3bb4523e31dcfcb66623d974f911
Shanmuga Priya <shanmugapriya6655@gmail.com>
2025-12-12 10:10:00 +0530

    fix db connection issue

--- src/routes/todoList.route.js
+++ src/routes/todoList.route.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { addTodo, viewTodo, completedTodo, editTodo, deleteItem } = require('../controllers/todoList.controller')
+const verifyToken = require('../middleware/authMiddleware')
 const router = express.Router()
 
-router.post('/addtodo', addTodo)
-router.get('/viewtodo', viewTodo)
-router.put('/completed', completedTodo)
-router.put('/edit', editTodo)
-router.delete('/delete/:id', deleteItem)
+// All todo routes require JWT authentication
+router.post('/addtodo', verifyToken, addTodo)
+router.get('/viewtodo', verifyToken, viewTodo)
+router.put('/completed', verifyToken, completedTodo)
+router.put('/edit', verifyToken, editTodo)
+router.delete('/delete/:id', verifyToken, deleteItem)
 
 module.exports = router
